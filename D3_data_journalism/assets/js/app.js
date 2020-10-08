0044ceb0e4806d5e222880f174d5153c1bc977aa
Jake Byford <jake_byford@outlook.com>
2020-10-08 19:38:23 -0400

    Created axis functions, appended axes to chart, created circles, axes labels

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,6 +1,6 @@
 // @TODO: YOUR CODE HERE!
 var svgWidth = 960;
-var svgHeight = 600;
+var svgHeight = 500;
 
 var margin = {
     top: 20,
@@ -18,28 +18,86 @@ var svg = d3.select(".chart")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-
     var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data 
 d3.csv("assets/data/data.csv").then(function(demoData) {
     console.log(demoData);
-});
 
 // Step 1: Parse Data from strings to numbers
 
-  demoData.forEach(function(data) {
-      data.income = +data.income;
-      data.obesity = +data.obesity;
-  });
+    demoData.forEach(function(data) {
+        data.income = +data.income;
+        data.obesity = +data.obesity;
+    });
 
 // Step 2: Create Scale functions
 
-  var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(demoData, d => d.income)])
-      .range([0, width]);
-  
-  var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(demoData, d => d.obesity)])
-      .range([height, 0]);
+    var xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(demoData, d => d.income)])
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(demoData, d => d.obesity)])
+        .range([height, 0]);
+
+
+// Step 3: Create axis functions
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+// Step 4: Append Axes to the chart
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+
+
+// Step 5: Create Circles
+
+    var circlesGroup = chartGroup.selectAll("stateCircle")
+        .data(demoData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.income))
+        .attr("cy", d => yLinearScale(d.obesity))
+        .attr("r", "15")
+        .attr("fill", "#89bdd3")
+        .attr("stroke", "#e3e3e3")
+        .text(function(d){return d.abbr});
+
+    var textCircle = chartGroup.selectAll('stateText')
+        .data(demoData)
+        .enter()
+        .append('text')
+        .text(function(d){return d.abbr})
+        .attr("x", d => xLinearScale(d.income / 1.0190))
+        .attr("y", d => yLinearScale(d.obesity -0.22 ))
+        .attr("fill", "#fff")
+        .attr("font-family", "sans-serif")
+
+// Create axes labels
+
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2 + 50))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Obesity (%)");
+
+    chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top +30})`)
+    .attr("class", "axisText")
+    .text("Household Income (Median)");
+
+});
+
+
+    
